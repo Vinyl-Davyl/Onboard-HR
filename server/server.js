78e20502d03551029190f8731cda74f0b97e7442
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-06-24 12:27:43 +0100

    feat: setup mvc

--- server/server.js
+++ server/server.js
@@ -4,12 +4,26 @@ const mongoose = require("mongoose");
 // helps passing any request coming from frontend
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middlewares(handling JSON data and data that comes via URL and sending data to client with bodyParser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes Middleware(all having prefix "api/users" then /register)
+app.use("/api/users", userRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 // Connect ot DB and start server
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
