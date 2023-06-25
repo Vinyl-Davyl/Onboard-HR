e500a7d28a89d0c523ef05281c0a97883c4c61fb
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-06-25 10:29:42 +0100

    feat: protect route middleware and function to get user profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -145,7 +145,22 @@ const logoutUser = asyncHandler(async (req, res) => {
 
 // get User -- *main
 const getUser = asyncHandler(async (req, res) => {
-  res.send("Get user Data");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
 });
 
 module.exports = {
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,10 +6,12 @@ const {
   logoutUser,
   getUser,
 } = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
-router.get("/getuser", getUser);
+// "protect" middleware in this route, gives getUser route access to "user" info "req.user = user"
+router.get("/getuser", protect, getUser);
 
 module.exports = router;
