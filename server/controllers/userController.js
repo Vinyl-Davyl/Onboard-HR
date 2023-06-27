c82614e687cd8c4e3458700b3e09b0ba7822687b
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-06-27 07:33:26 +0100

    update: controllers for change password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -211,7 +211,32 @@ const updateUser = asyncHandler(async (req, res) => {
 
 // Change Password -- *main
 const changePassword = asyncHandler(async (req, res) => {
-  res.send("Password Changed");
+  const user = await User.findById(req.user._id);
+
+  const { oldPassword, password } = req.body;
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, please signup");
+  }
+  // Validate
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error("Please add old and new password");
+  }
+
+  // Check if old password matches password in DB
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  // Save new password
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+    res.status(200).send("Password changed successful");
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect");
+  }
 });
 
 module.exports = {
