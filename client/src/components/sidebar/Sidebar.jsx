85375f4bdbfccc5ccc1a934fd47b209a354b385a
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-07-02 22:56:24 +0100

    update: further update on sidebar setup

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -4,17 +4,27 @@ import { BiExclude } from "react-icons/bi";
 import { HiMenuAlt3 } from "react-icons/hi";
 import menu from "../../data/sidebar";
 import SidebarItem from "./SidebarItem";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(true);
   const toggle = () => setIsOpen(!isOpen);
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
 
   return (
     <div className="layout">
       <div className="sidebar" style={{ width: isOpen ? "230px" : "60px" }}>
         <div className="top_section">
           <div className="logo" style={{ display: isOpen ? "block" : "none" }}>
-            <BiExclude size={35} style={{ cursor: "pointer" }} />
+            <BiExclude
+              size={35}
+              style={{ cursor: "pointer" }}
+              onClick={goHome}
+            />
           </div>
           <div
             className="bars"
@@ -28,8 +38,18 @@ const Sidebar = ({ children }) => {
             />
           </div>
         </div>
+        {menu.map((item, index) => {
+          return <SidebarItem key={index} item={item} isOpen={isOpen} />;
+        })}
       </div>
-      <main>{children}</main>
+      <main
+        style={{
+          paddingLeft: isOpen ? "230px" : "60px",
+          transition: "all .5s",
+        }}
+      >
+        {children}
+      </main>
     </div>
   );
 };
