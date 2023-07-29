432aaf83782f9b57b70ed68d0c57274688f58704
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-07-30 00:19:19 +0100

    feat: get all employees implementation and rendering on dashboard (#51)
    * feat: create employe and save to DB
    
    * feat: create get all products function using redux managing state
    
    * feat: update on getting all employees
    
    * feat: reconfigure redux store function and get all employees correctly
    
    * feat: render employees on dashboard
    
    * fix
    
    * update: packages
    
    ---------

--- client/src/redux/features/employee/employeeService.jsx
+++ client/src/redux/features/employee/employeeService.jsx
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-const API_URL = `${BACKEND_URL}/api/products`;
+const API_URL = `${BACKEND_URL}/api/employees`;
 
 //  Create New Employee(not using try catch to execute request, createAsyncThunk instead)
 const createEmployee = async (formData) => {
@@ -11,8 +11,15 @@ const createEmployee = async (formData) => {
   return response.data;
 };
 
+// Get all Employees
+const getEmployees = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
 const employeeService = {
   createEmployee,
+  getEmployees,
 };
 
 // Alternatively
