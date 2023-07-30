c350e63a385f877429c76ef2f1993abd09115658
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2023-07-30 19:19:29 +0100

    update: added employee infoBox reusable component

--- client/src/components/employee/employeeSummary/EmployeeSummary.jsx
+++ client/src/components/employee/employeeSummary/EmployeeSummary.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import "./EmployeeSummary.scss";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { BsCart4, BsCartX } from "react-icons/bs";
+import { BiCategory } from "react-icons/bi";
 
 const EmployeeSummary = () => {
   return <div>EmployeeSummary</div>;
