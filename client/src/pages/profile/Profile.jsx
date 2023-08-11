e27c8a83e9e4648b21b69d514011e64db415b662
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2023-08-11 06:29:53 +0100

    update: fetched getProfile from API on authservices and called on redux to get profile data

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Profile.scss";
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
+import { useDispatch } from "react-redux";
+import { getUser } from "../../services/authService";
+import { SET_NAME, SET_USER } from "../../redux/features/authSlice";
 
 const Profile = () => {
+  // redirect if user is logged out
+  useRedirectLoggedOutUser("/login");
+  const dispatch = useDispatch();
+
+  const [profile, setProfile] = useState(null);
+  const [isLoading, selectIsLoading] = useState(null);
+
+  //   fetch from auth service getProfile api and call on redux
+  useEffect(() => {
+    selectIsLoading(true);
+    async function getUserData() {
+      // calling from api
+      const data = await getUser();
+      console.log(data);
+
+      setProfile(data);
+      selectIsLoading(false);
+      await dispatch(SET_USER(data));
+      await dispatch(SET_NAME(data.name));
+    }
+    // call function
+    getUserData();
+  }, [dispatch]);
+
   return <div>Profile</div>;
 };
 
